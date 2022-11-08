3bf6986844f61b6f2e8c1f6dbf119471a13484ea
Bekzat Bakirov <bekzat.bakirov.alt@gmail.com>
2022-11-08 21:34:13 +0600

    first commit

---
+++ registration/automatize.js
@@ -0,0 +1,17 @@
+const page1 = document.querySelector("#registration")
+
+const page2 = document.querySelector("#mchoise")
+
+const page3 = document.querySelector("#tchoise")
+
+const button = document.querySelector("#registration")
+
+const button2 = document.querySelector("#mchoice")
+
+console.log(button2);
+
+button.addEventListener("click", nextPage)
+
+function nextPage() {
+    
+}
